1b280f1e057d4f767d6cbf57ac43fab1f13bd22d
Serega <mail@bobrus.su>
2016-04-06 21:07:00 +0300

    add first test

---
+++ src/test/java/teamwork/testBomber.java
@@ -0,0 +1,19 @@
+package teamwork;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Serega on 06.04.2016.
+ */
+public class testBomber {
+
+    @Test
+    public void testCreate() {
+        assertNotNull(new Bomber());
+    }
+
+
+}
